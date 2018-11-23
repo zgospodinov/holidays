226923059f7fa5147589851623e5f517f1dfdc55
zdravko.gospodinov <zdravko.gospodinov@gmail.com>
2018-11-23 19:36:58 +0200

    updating

--- app.js
+++ app.js
@@ -10,11 +10,23 @@ function changeTitle() {
 };
 
 $(document).ready(function () {
-    $("#fetchBtn").click(function (e) {
-        console.log("Getting holidays");
-        // fetchData();
 
-        var holidayUrl = `https://holidayapi.com/v1/holidays?key=e2a15814-aca6-4e89-a057-d658e84035a9&country=BG&year=2017&month=12`;
+    $("#fetchBtn").click(function (e) {
+        var apiKey = "a777a81982d82a4fa8d82983aae77c9ded59de6a";
+        var country, year;
+
+        console.log("Getting holidays");
+
+        var select = document.getElementById("inputGroupSelect03").value;
+        if (select === "1") {
+            document.title = "Holidays-BG"
+        } else if (select === "2") {
+            document.title = "Holidays-DE"
+        }
+
+        // fetchData();
+        // var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=${country}&year=${year}&api_key=${apiKey}`;
+        var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=BG&year=2019&api_key=${apiKey}`;
         var jax = $.ajax({
             url: holidayUrl,
             type: "GET",
@@ -25,4 +37,6 @@ $(document).ready(function () {
         });
 
     });
+
+
 });
