557a70b037e45522bdace1cdde96ff9a945e3907
zdravki4 <zdravko.gospodinov@gmail.com>
2018-11-26 07:53:19 +0200

    url request ready

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
-function changeTitle() {
-    var select = document.getElementById("inputGroupSelect03").value;
+function changeTitle () {
+  var select = document.getElementById('inputGroupSelect03').value
 
-    if (select === "1") {
-        document.title = "Holidays-BG"
-    } else if (select === "2") {
-        document.title = "Holidays-DE"
-    }
-    console.log(select);
-};
+  if (select === 'BG') {
+    document.title = 'Holidays-BG'
+  } else if (select === 'BG') {
+    document.title = 'Holidays-DE'
+  }
+  console.log(select)
+}
 
 $(document).ready(function () {
+  $('#fetchBtn').click(function (e) {
+    var apiKey = 'a777a81982d82a4fa8d82983aae77c9ded59de6a'
+    var country, year
 
-    $("#fetchBtn").click(function (e) {
-        var apiKey = "a777a81982d82a4fa8d82983aae77c9ded59de6a";
-        var country, year;
+    console.log('Getting holidays')
 
-        console.log("Getting holidays");
+    var country = document.getElementById('inputGroupSelect03').value;  
+    var year = document.getElementById('inputGroupSelect04').value;  
+    console.log(year);
 
-        var select = document.getElementById("inputGroupSelect03").value;
-        if (select === "1") {
-            document.title = "Holidays-BG"
-        } else if (select === "2") {
-            document.title = "Holidays-DE"
+    var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=${country}&year=${year}&api_key=${apiKey}`
+    var jax = $.ajax({
+      url: holidayUrl,
+      type: 'GET',
+      success: function (json) {
+        // console.log(json)
+        var holidays = json.response.holidays
+        // console.log(holidays)
+
+        var tableBody = $('#tbody')
+        tableBody.empty()
+
+        for (let i = 0, len = holidays.length; i < len; i++) {
+          var holiday = holidays[i]
+          var date = holiday.date
+          var name = holiday.name
+
+          var holidayRecord =
+          `<tr>
+                <td>${date}</td>
+                <td>${name}</td>
+            </tr>`
+
+          tableBody.append(holidayRecord)
         }
+      }
 
-        // fetchData();
-        // var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=${country}&year=${year}&api_key=${apiKey}`;
-        var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=BG&year=2019&api_key=${apiKey}`;
-        var jax = $.ajax({
-            url: holidayUrl,
-            type: "GET",
-            success: function (xml) {
-                console.log(xml);
-            }
-
-        });
-
-    });
-
-
-});
+    })
+  })
+})
