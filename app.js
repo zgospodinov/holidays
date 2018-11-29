003286cc449bb1da0ad28583a8495d3faf6ebab7
Zdravko Gospodinov <zdravki4@gmail.com>
2018-11-29 14:20:58 +0200

    FakeAPI ready

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var countries = {
   BG: "Bulgaria",
   DE: "Germany"
 }
-
+var testing = true;
 var mockupResponse = {
   "meta": {
     "code": 200
@@ -147,61 +147,104 @@ function changeTitle() {
 
 }
 
-$(document).ready(function () {
-  $('#fetchBtn').click(function (e) {
-    var apiKey = 'a777a81982d82a4fa8d82983aae77c9ded59de6a'
-    var country, year
+if (testing !== true) {
+  console.log("Connecting to API")
+  $(document).ready(function () {
+    $('#fetchBtn').click(function (e) {
+      var apiKey = 'a777a81982d82a4fa8d82983aae77c9ded59de6a'
+      var country, year
 
-    // console.log('Getting holidays')
+      // console.log('Getting holidays')
 
-    var country = document.getElementById('inputGroupSelect03').value;
-    var year = document.getElementById('inputGroupSelect04').value;
-    // console.log(year);
+      var country = document.getElementById('inputGroupSelect03').value;
+      var year = document.getElementById('inputGroupSelect04').value;
+      // console.log(year);
 
-    var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=${country}&year=${year}&api_key=${apiKey}`
-    var jax = $.ajax({
-      url: holidayUrl,
-      type: 'GET',
-      success: function (json) {
-        // console.log(json)
-        var holidays = json.response.holidays
-        // console.log(holidays[0])        
+      var holidayUrl = `https://www.calendarindex.com/api/v1/holidays?country=${country}&year=${year}&api_key=${apiKey}`
+      var jax = $.ajax({
+        url: holidayUrl,
+        type: 'GET',
+        success: function (json) {
+          // console.log(json)
+          var holidays = json.response.holidays
+          // console.log(holidays[0])        
 
-        var tableBody = $('#tbody')
-        tableBody.empty()
+          var tableBody = $('#tbody')
+          tableBody.empty()
 
-        for (let i = 0, len = holidays.length; i < len; i++) {
-          var holiday = holidays[i]
-          var d = new Date(holiday.date)
-          // console.log(d.getMonth().toString().length);
+          for (let i = 0, len = holidays.length; i < len; i++) {
+            var holiday = holidays[i]
+            var d = new Date(holiday.date)
+            // console.log(d.getMonth().toString().length);
 
-          var monthString;
-          if ((d.getMonth() + 1).toString().length > 1) {
-            monthString = (d.getMonth() + 1);
-            console.log(monthString);
-          } else {
-            monthString = `0${(d.getMonth() + 1)}`;
-            console.log(monthString);
-          }
+            var monthString;
+            if ((d.getMonth() + 1).toString().length > 1) {
+              monthString = (d.getMonth() + 1);
+              // console.log(monthString);
+            } else {
+              monthString = `0${(d.getMonth() + 1)}`;
+              // console.log(monthString);
+            }
 
-          // var dateFormatted = d.getDate() + '/' + (d.getMonth() + 1);
-          var dateFormatted = d.getDate() + '/' + monthString;
+            // var dateFormatted = d.getDate() + '/' + (d.getMonth() + 1);
+            var dateFormatted = d.getDate() + '/' + monthString;
 
-          var name = holiday.name
+            var name = holiday.name
 
-          var holidayRecord =
-            `<tr>
+            var holidayRecord =
+              `<tr>
                 <td>${dateFormatted}</td>
                 <td>${name}</td>
             </tr>`;
 
-          tableBody.append(holidayRecord)
+            tableBody.append(holidayRecord)
+          }
         }
-      }
 
+      })
     })
   })
-})
+} else {
+  console.log("Connecting to FAKE")
+  fakeCAll(mockupResponse);
+}
+
+function fakeCAll(json) {
+  // console.log(json)
+  var holidays = json.response.holidays
+  // console.log(holidays[0])        
+
+  var tableBody = $('#tbody')
+  tableBody.empty()
+
+  for (let i = 0, len = holidays.length; i < len; i++) {
+    var holiday = holidays[i]
+    var d = new Date(holiday.date)
+    // console.log(d.getMonth().toString().length);
+
+    var monthString;
+    if ((d.getMonth() + 1).toString().length > 1) {
+      monthString = (d.getMonth() + 1);
+      // console.log(monthString);
+    } else {
+      monthString = `0${(d.getMonth() + 1)}`;
+      // console.log(monthString);
+    }
+
+    // var dateFormatted = d.getDate() + '/' + (d.getMonth() + 1);
+    var dateFormatted = d.getDate() + '/' + monthString;
+
+    var name = holiday.name
+
+    var holidayRecord =
+      `<tr>
+          <td>${dateFormatted}</td>
+          <td>${name}</td>
+      </tr>`;
+
+    tableBody.append(holidayRecord)
+  }
+}
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
