4810a3d98e06f2f3ff451ad9cbdd3608147de59b
Zdravko Gospodinov <zdravki4@gmail.com>
2018-11-29 15:42:16 +0200

    Revert "Adjustted scrollTop to manage mobile devices"
    This reverts commit 0e8ef3238c8820279adab31038b9480bdeb242bc.

--- app.js
+++ app.js
@@ -252,7 +252,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("myBtn").style.display = "none";
@@ -261,44 +261,13 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  // // console.log($('body'));
-  // $('body').css('overflow-y', 'hidden');
-  // $('body').animate({
+  // console.log($('body'));
+  $('body').css('overflow-y', 'hidden');
+  $('body').animate({
+    scrollTop: 0
+  }, 1000);
+  $('body').css('overflow-y', 'auto');
+  // $('documentElement').animate({
   //   scrollTop: 0
   // }, 1000);
-  // $('body').css('overflow-y', 'auto');
-  // // $('documentElement').animate({
-  // //   scrollTop: 0
-  // // }, 1000);
-  scrollToCustom(document.body, 0, 1000);
-
 }
-
-// https://gist.github.com/andjosh/6764939
-function scrollToCustom(element, to, duration) {
-  var start = element.scrollTop,
-    change = to - start,
-    currentTime = 0,
-    increment = 20;
-
-  var animateScroll = function () {
-    currentTime += increment;
-    var val = Math.easeInOutQuad(currentTime, start, change, duration);
-    element.scrollTop = val;
-    if (currentTime < duration) {
-      setTimeout(animateScroll, increment);
-    }
-  };
-  animateScroll();
-}
-
-//t = current time
-//b = start value
-//c = change in value
-//d = duration
-Math.easeInOutQuad = function (t, b, c, d) {
-  t /= d / 2;
-  if (t < 1) return c / 2 * t * t + b;
-  t--;
-  return -c / 2 * (t * (t - 2) - 1) + b;
-};
