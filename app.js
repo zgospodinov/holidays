fb77babdfbb293c71530365f6661763b34c9c1b8
Zdravko Gospodinov <zdravki4@gmail.com>
2018-11-29 12:54:01 +0200

    Scroll to top button
    And minor css changes

--- app.js
+++ app.js
@@ -64,3 +64,22 @@ $(document).ready(function () {
     })
   })
 })
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
